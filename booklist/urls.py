c7f86f2ffeca5c35427c9584d2388f29074cecb3
overment <patrix9811@gmail.com>
2018-11-13 10:52:20 +0100

    Permission for user was added

--- booklist/urls.py
+++ booklist/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
+
 from . import views
+
 
 router = DefaultRouter()
 router.register(r'books', views.BookViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls))
+    url(r'^', include(router.urls)),
 ]
